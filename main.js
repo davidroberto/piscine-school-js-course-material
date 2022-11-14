be1ee0e71b35a07a9f6e291883e3c5bfef1988a4
David Robert <contact@david-robert.fr>
2022-11-14 14:31:51 +0100

    js paid content with classes

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const btn = document.querySelector('.js-btn');
 btn.addEventListener('click', () => {
     const content = document.querySelector('.paid-content');
 
-    content.style.display = 'block';
-    btn.style.display = 'none';
+    // content.style.display = 'block';
+    // btn.style.display = 'none';
+
+    content.classList.remove('paid-content');
+    btn.classList.add('d-n');
 })
