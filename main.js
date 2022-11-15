b067725224d628e27829b8f8aa6b7baa134055db
David Robert <contact@david-robert.fr>
2022-11-15 16:41:12 +0100

    refacto functions

--- main.js
+++ main.js
@@ -7,14 +7,54 @@ const fetchMeals = async () => {
     return mealsData;
 }
 
+const createPElement = (textToDisplay, container) => {
+    const pElement = document.createElement('p');
+    pElement.textContent = textToDisplay;
+    container.append(pElement);
+}
+
+const createH2Element = (textToDisplay, container) => {
+    const h2Element = document.createElement('h2');
+    h2Element.textContent = textToDisplay;
+    container.append(h2Element);
+}
+
+const createImgElement = (imageToDisplay, container) => {
+    const imgElement = document.createElement('img');
+    imgElement.setAttribute('src', imageToDisplay);
+    container.append(imgElement);
+}
+
+const createUlElement = (container) => {
+    const ulElement = document.createElement('ul');
+    container.append(ulElement);
+}
+
+
 const displayMeals = async() => {
     const mealsData = await fetchMeals();
     const container = document.querySelector('#root');
 
     mealsData.meals.map((mealData) => {
-        const h2Element = document.createElement('h2');
-        h2Element.textContent = mealData.strMeal;
-        container.append(h2Element);
+
+        createH2Element(mealData.strMeal, container);
+        createImgElement(mealData.strMealThumb, container);
+        createPElement(mealData.strCategory, container);
+        createPElement(mealData.strInstructions, container);
+        createPElement(mealData.strTags, container);
+        createUlElement(container);
+
+
+        for(let i = 1; i <= 20; i++) {
+
+            if (mealData["strIngredient" + i]) {
+                const liElement = document.createElement('li');
+                liElement.textContent = mealData["strIngredient" + i];
+                ulElement.append(liElement);
+            }
+
+        }
+
     })
 }
 
