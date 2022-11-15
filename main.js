1b2f04b0e1e6f5bc93b716f2aa3af54099e129af
David Robert <contact@david-robert.fr>
2022-11-15 10:07:37 +0100

    fetch without comments

--- main.js
+++ main.js
@@ -8,25 +8,12 @@ const fetchMeals = async () => {
 }
 
 const displayMeals = async() => {
-
-    //je fais un appel "fetch" pour récupérer des données depuis une API
     const mealsData = await fetchMeals();
-
-    // je cible la div dans le HTML qui possède l'id root
     const container = document.querySelector('#root');
 
-    // je fais une boucle sur les données récupérées depuis l'API
-    // grâce à la fonction map (on pourrait utiliser for ou forEach)
     mealsData.meals.map((mealData) => {
-
-        // pour chaque recette de cuisine
-        // je créé un élément HTML h2
         const h2Element = document.createElement('h2');
-
-        // je passe en titre de l'élément le nom de la recette de cuisine
         h2Element.textContent = mealData.strMeal;
-
-        // j'insère l'élément dans la div qui a l'id #root
         container.append(h2Element);
     })
 }
@@ -34,9 +21,3 @@ const displayMeals = async() => {
 
 displayMeals();
 
-
-
-
-
-// récupérer les données fournies par cette api
-// les afficher en js dans la div avec l'id root
