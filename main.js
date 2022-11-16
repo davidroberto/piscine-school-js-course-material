84ba5e9dfa7c857a9c7009fd7ffb8a3b864c3aa9
David Robert <contact@david-robert.fr>
2022-11-16 17:10:57 +0100

    router js

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+const container = document.querySelector('#root');
+
+const btnMeals = document.querySelector('.meals-btn');
+
+btnMeals.addEventListener('click', () => {
+    const queryParams = new URLSearchParams(window.location.search);
+    queryParams.set("page", "meals");
+    history.replaceState(null, null, "?"+queryParams.toString());
+    
+    container.innerHTML = "";
+    renderMeals();
+});
+
+
+const btnCategories = document.querySelector('.categories-btn');
+
+btnCategories.addEventListener('click', () => {
+    const queryParams = new URLSearchParams(window.location.search);
+    queryParams.set("page", "categories");
+    history.replaceState(null, null, "?"+queryParams.toString());
+    
+    container.innerHTML = "";
+    renderCategories();
+});
+
 
 // à laisser de côté pour l'instant
 const fetchMeals = async () => {
@@ -32,51 +57,58 @@ const createImgElement = (imageToDisplay, container) => {
     container.append(imgElement);
 }
 
-const displayMeals = async() => {
-    const container = document.querySelector('#root');
+const renderMeals = async () => {
+    const mealsData = await fetchMeals();
+
+    mealsData.meals.map((mealData) => {
+
+        createH2Element(mealData.strMeal, container);
+        createImgElement(mealData.strMealThumb, container);
+        createPElement(mealData.strCategory, container);
+        createPElement(mealData.strInstructions, container);
+        createPElement(mealData.strTags, container);
+
+        const ulElement = document.createElement('ul');
+        container.append(ulElement);
+
+
+        for(let i = 1; i <= 20; i++) {
+
+            if (mealData["strIngredient" + i]) {
+                const liElement = document.createElement('li');
+                liElement.textContent = mealData["strIngredient" + i];
+                ulElement.append(liElement);
+            }
+
+        }
+
+    })
+}
+
+const renderCategories = async() => {
+    const categoriesData = await fetchCategories();
+
+    categoriesData.categories.map((categoryData) => {
+        createH2Element(categoryData.strCategory, container);
+    });
+}
+
+
+const renderApp = async() => {
 
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const page = urlParams.get('page');
     
-
     if (page === "meals") {
-        const mealsData = await fetchMeals();
-
-        mealsData.meals.map((mealData) => {
-
-            createH2Element(mealData.strMeal, container);
-            createImgElement(mealData.strMealThumb, container);
-            createPElement(mealData.strCategory, container);
-            createPElement(mealData.strInstructions, container);
-            createPElement(mealData.strTags, container);
-    
-            const ulElement = document.createElement('ul');
-            container.append(ulElement);
-    
-    
-            for(let i = 1; i <= 20; i++) {
-    
-                if (mealData["strIngredient" + i]) {
-                    const liElement = document.createElement('li');
-                    liElement.textContent = mealData["strIngredient" + i];
-                    ulElement.append(liElement);
-                }
-    
-            }
-    
-        })
+        renderMeals();
     } else if(page === "categories") {
-        const categoriesData = await fetchCategories();
-
-        categoriesData.categories.map((categoryData) => {
-            createH2Element(categoryData.strCategory, container);
-        });
+        renderCategories();
+    } else if (!page) {
+        createH2Element('Site de recettes de cuisine', container);
+    } else {
+        createH2Element('404 : page introuvable', container);
     }
-
-    
 }
 
-
-displayMeals();
-
+renderApp();
